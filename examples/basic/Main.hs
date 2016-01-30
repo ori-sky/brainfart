9fb23382662b0c0617df09ebbca8cf3b6ab332f8
Shockk <shokku.ra@gmail.com>
2016-01-30 04:13:10 +0000

    Work on UI including CSS

--- examples/basic/Main.hs
+++ examples/basic/Main.hs
@@ -7,24 +7,35 @@
 
 module Main where
 
-import Data.List (intercalate, genericReplicate)
+import Data.Foldable (traverse_)
 import Data.Text (Text)
 import Data.Default
-import qualified Data.Map as M
-import Text.Blaze.Html5 (Html, (!), toHtml)
-import Text.Blaze.Html5.Attributes (href)
-import qualified Text.Blaze.Html5 as H
-import Happstack.Foundation
 import Data.Memory.Types
 import Data.Memory.DB.Acid
+import Text.Blaze.Html5 (Html, (!), toHtml)
+import Clay ((?))
+import Happstack.Foundation
+import qualified Data.Map as M
+import qualified Data.Text.Lazy.Encoding as LazyT
+import qualified Data.ByteString.Char8 as B
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import qualified Clay as C
 
 instance PathInfo a => PathInfo [a] where
     toPathSegments = concat . fmap toPathSegments
     fromPathSegments = many fromPathSegments
 
+instance ToMessage C.Css where
+    toContentType _ = B.pack "text/css; charset=UTF-8"
+    toMessage = LazyT.encodeUtf8 . C.render
+
 data Route = Search
+           | Css
            | NewNode [NodeID] Text
            | ViewNode [NodeID]
+
+$(derivePathInfo ''Route)
 
 type Memory'    = FoundationT' Route MemoryAcid () IO
 type Memory     = XMLGenT Memory'
@@ -33,30 +44,39 @@ type MemoryForm = FoundationForm Route MemoryAcid () IO
 instance (Functor m, Monad m) => HasAcidState (FoundationT' url MemoryAcid s m) Node where
     getAcidState = acidMemory <$> getAcidSt
 
-$(derivePathInfo ''Route)
-
-template :: Text -> Html -> Html
-template title body = H.docTypeHtml $ do
-    H.head $ do
-        H.title (toHtml title)
-    H.body $ do
-        body
-
-outputNode :: Integer -> Node -> [String]
-outputNode indent (Node nData nMap) = this : rest
-  where this = genericReplicate indent ' ' ++ show (nodeText nData)
-        rest = concatMap (outputNode (indent + 2) . snd) (M.toAscList nMap)
+outputNode :: Node -> Html
+outputNode (Node nData nMap) = H.div ! A.class_ "box" $ do
+    toHtml (nodeText nData)
+    traverse_ (outputNode . snd) (M.toAscList nMap)
 
 route :: Route -> Memory Response
 route Search = ok $ toResponse $ template "Search" $ H.h1 "Search"
 route (NewNode xs text) = update (UInsertNode node xs) >>= \case
     Nothing   -> internalServerError $ toResponse $ template "Internal Server Error" $ "Failed to insert node"
-    Just node -> ok                  $ toResponse $ template "New Node"  $ H.pre $ toHtml $ intercalate "\n" (outputNode 0 node)
+    Just node -> ok                  $ toResponse $ template "New Node"  $ H.pre (outputNode node)
   where node = def { nodeData = def { nodeText = text } }
 route (ViewNode xs) = query (QLookupNode xs) >>= \case
     Nothing   -> internalServerError $ toResponse $ template "Internal Server Error" $ "Failed to lookup node"
-    Just node -> ok                  $ toResponse $ template "View Node" $ H.pre $ toHtml $ intercalate "\n" (outputNode 0 node)
+    Just node -> ok                  $ toResponse $ template "View Node" $ H.pre (outputNode node)
+route Css = ok (toResponse css)
 
 main :: IO ()
 main = withAcid $ \root -> do
     simpleApp id defaultConf (AcidUsing root) () Search "" route
+
+template :: Text -> Html -> Html
+template title body = H.docTypeHtml $ do
+    H.head $ do
+        H.title (toHtml title)
+        H.link
+            ! A.type_ "text/css"
+            ! A.rel "stylesheet"
+            ! A.href (H.toValue $ toPathInfo Css)
+    H.body $ do
+        body
+
+css :: C.Css
+css = do
+    ".box" ? do
+        C.padding (C.px 3) (C.px 5) (C.px 3) (C.px 5)
+        C.background (C.rgba 20 24 30 10)
